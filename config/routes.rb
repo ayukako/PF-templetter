372f02ca4221085e1984c37e88fb77504909e305
ayukako <yukako1102n@gmail.com>
2022-05-26 10:57:46 +0000

    add all_routing

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,44 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :public do
+    get 'homes/top'
+   end
+
+  namespace :admin do
+    resources :all_items, only: [:index]
+    resources :envelopes, except:[:edit, :update, :index]
+    resources :papers, except: [:edit, :update, :index]
+    resources :fonts, except: [:edit, :update, :index]
+    resources :articles, except: [:index]
+    resources :orders, only: [:show, :update]
+  end
+  namespace :admin do
+    get 'homes/top'
+    root to: "homes#top"
+  end
+
+  namespace :public do
+    resource :items, except: [:index, :destroy] do
+      collection do
+        get :destroy_all
+      end
+    end
+    resource :customers, only: [:show] do
+      collection do
+        get :unsubscribe
+        patch :withdraw
+      end
+    end
+    resources :orders, only: [:new, :create, :show] do
+      collection do
+        post :confirm
+        get :thanks
+      end
+    end
+    resources :items_choices, only: [:index]
+  end
+
+
 end
