559728273a31961f4b5a9a70e913301b7db8f0f2
ayukako <yukako1102n@gmail.com>
2022-05-26 14:31:53 +0000

    add 新規投稿機能（封筒、便箋、フォント、文章）

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,8 +1,15 @@
 class Admin::ArticlesController < ApplicationController
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+    redirect_to admin_items_path(@article)
+    else
+    render :new
+    end
   end
 
   def show
@@ -15,5 +22,9 @@ class Admin::ArticlesController < ApplicationController
   end
 
   def update
+  end
+
+  def article_params
+    params.require(:article).permit(:name, :image, :content)
   end
 end
--- app/controllers/admin/envelopes_controller.rb
+++ app/controllers/admin/envelopes_controller.rb
@@ -1,13 +1,24 @@
 class Admin::EnvelopesController < ApplicationController
   def new
+    @envelope = Envelope.new
   end
 
   def create
+    @envelope = Envelope.new(envelope_params)
+    if @envelope.save
+    redirect_to admin_items_path(@envelope)
+    else
+    render :new
+    end
   end
 
   def show
   end
 
   def destroy
+  end
+
+  def envelope_params
+    params.require(:envelope).permit(:name, :image)
   end
 end
