9ebe74fc495d2f8b5337a1d2df3e40fb07fe12a5
ayukako <yukako1102n@gmail.com>
2022-05-29 08:48:21 +0000

    update header(

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+
+
+   before_action :configure_sign_up_params, only: [:create]
+
+
+  def after_sign_up_path_for(resource)
+    public_customers_path
+  end
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -38,12 +45,12 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+   protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  # # If you have extra params to permit, append them to the sanitizer.
+   def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :postal_code, :address,:email])
+   end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
