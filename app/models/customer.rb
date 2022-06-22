0f9730a1103ea69090085e84be3b2cea09806070
ayukako <yukako1102n@gmail.com>
2022-06-22 13:43:43 +0000

    addレスポンシブ対応

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,10 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
 
 
   has_many :orders
